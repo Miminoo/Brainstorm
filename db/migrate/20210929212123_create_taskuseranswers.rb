ece5674121ffaf651b449e67833067d7e7b538c9
isMimino <lokaloka5489318@gmail.com>
2021-10-01 15:45:31 +0300

    fix

--- db/migrate/20210929212123_create_taskuseranswers.rb
+++ db/migrate/20210929212123_create_taskuseranswers.rb
@@ -3,7 +3,6 @@ class CreateTaskuseranswers < ActiveRecord::Migration[6.1]
     create_table :taskuseranswers do |t|
       t.string :name
       t.integer :user_id
-      t.boolean :status, null: true, default: false
       t.belongs_to :task, null: false, foreign_key: true
       t.timestamps
     end
