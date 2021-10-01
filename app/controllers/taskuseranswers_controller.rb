8f1a278c19fff47bc1005279e7da96eb4897d6c9
isMimino <lokaloka5489318@gmail.com>
2021-10-01 15:20:03 +0300

    Finished with problem solving

--- app/controllers/taskuseranswers_controller.rb
+++ app/controllers/taskuseranswers_controller.rb
@@ -2,7 +2,15 @@ class TaskuseranswersController < ApplicationController
 
   def create
     @task = Task.find(params[:task_id])
+    @current_user ||= User.find_by_id(params[:user_id]) || User.find_by_id(params[:id])
     @taskuseranswer = @task.taskuseranswers.create(taskuseranswer_params)
+    if current_user.id == @taskuseranswer.user_id
+        @task.answers.each do |task|
+        if task.name == @taskuseranswer.name
+          @current_user.update_attribute(:complete_tasks, @current_user.complete_tasks +=1)
+        end
+      end
+    end
     redirect_to task_path(@task)
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -108,6 +108,7 @@ ActiveRecord::Schema.define(version: 2021_09_29_212123) do
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "name", default: "", null: false
+    t.integer "complete_tasks", default: 0
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
