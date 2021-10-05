813223cf86c0bd952573c972ebf111d3242b0ad7
isMimino <lokaloka5489318@gmail.com>
2021-10-05 12:07:00 +0300

    create cloud_tags

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,9 @@
 class Tag < ActiveRecord::Base
   has_many :taggings
-  has_many :tasks, through: :taggings 
+  has_many :tasks, through: :taggings
+
+  def self.counts
+    Tag.select("name, count(taggings.tag_id) as count").joins(:taggings).group("taggings.tag_id")
+  end
+
 end
