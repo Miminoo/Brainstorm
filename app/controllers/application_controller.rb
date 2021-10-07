9ad32ec32983735d425f382d4234b2291e1cfb40
isMimino <lokaloka5489318@gmail.com>
2021-10-07 21:44:26 +0300

    added saving users localize

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,22 +1,35 @@
 class ApplicationController < ActionController::Base
   
   around_action :swich_locale
-
+  def index
+    @user = User.find_by_id(current_user.id)
+  end
   private
 
   def swich_locale(&action)
     locale = params[:locale] || I18n.default_locale
-    I18n.with_locale locale, &action
+    if user_signed_in?
+      @user = User.find_by_id(current_user.id)
+      @user.update_attribute(:locale, locale)
+      I18n.with_locale @user.locale, &action
+    else
+      I18n.with_locale locale, &action
+    end
   end
 
   def locale_from_url
     locale = params[:locale]
-
     return locale if I18n.available_locales.map(&:to_s).include?(locale)
     nill
   end
 
   def default_url_options
+    if user_signed_in?
+      @user = User.find_by_id(current_user.id)
+      { locale: @user.locale }
+    else
     { locale: I18n.locale }
+    end
   end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_03_113446) do
+ActiveRecord::Schema.define(version: 2021_10_07_150419) do
 
   create_table "active_storage_attachments", charset: "utf8mb4", force: :cascade do |t|
     t.string "name", null: false
@@ -123,6 +123,7 @@ ActiveRecord::Schema.define(version: 2021_10_03_113446) do
     t.string "provider", limit: 50, default: "", null: false
     t.string "uid", limit: 500, default: "", null: false
     t.boolean "admin"
+    t.string "locale", default: "ru"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
