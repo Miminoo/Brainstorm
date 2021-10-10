2ce2f5678a1dcd7f89fdb7773bc2ce022b4edba1
isMimino <lokaloka5489318@gmail.com>
2021-10-10 14:18:50 +0300

    add def google

--- app/controllers/omniauth_controller.rb
+++ app/controllers/omniauth_controller.rb
@@ -10,6 +10,16 @@ class OmniauthController < ApplicationController
     end
   end
 
+  def google_oauth2
+    @user = User.create_from_google_data(request.env['omniauth.auth'])
+    if @user.persisted?
+      sign_in_and_redirect @user
+    else
+      flash[:error] = 'There was a problem signing you in through Google. Please register or try signing in later.'
+      redirect_to new_user_session_path
+    end
+  end
+
   def failure
     flash[:error] = 'There was a problem signing you in. Please register or try signing in later.' 
     redirect_to new_user_session_path
